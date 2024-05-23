ddcea19f7feecd30107fad63b306734bee165756
Shreyas kulkarni <68956403+Shreyas-6403@users.noreply.github.com>
2024-05-23 15:45:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -39,9 +39,7 @@ def calculate_profit_loss(data, quantity_sold_col, unit_price_col):
 # Display the image and title side by side
 col1, col2 = st.columns([1, 3])
 col1.image('sales.jpg', width=150)
-col2.title("PRODUCT
-SALES
-ANALYSIS")
+col2.title("PRODUCT SALES ANALYSIS")
 
 # User input form
 st.header('Input Parameters')
