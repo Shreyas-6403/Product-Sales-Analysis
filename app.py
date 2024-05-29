1a258ab9b515450b9a7f91dca5d7328ceb5dfbf6
Shreyas kulkarni <68956403+Shreyas-6403@users.noreply.github.com>
2024-05-29 23:09:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def train_model(data):
     else:
         # Train-test split
         try:
-            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
         except ValueError as e:
             st.error(f"ValueError during train_test_split: {e}")
             return None
